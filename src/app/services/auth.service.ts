039705b1df97d223ce52c6ae257068fdb9148d84
JitBherwani <jitbherwani92@gmail.com>
2023-01-28 20:50:00 +0530

    initial commit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -19,11 +19,11 @@ export class AuthService {
 
 
   signup(data:any):Observable<any>{
-    return this.http.post(`${environment.apiUrl}/signup`,data)
+    return this.http.post(`${environment.apiUrl}/user/signup`,data)
   }
 
   signin(data:any):Observable<any>{
-    return this.http.post(`${environment.apiUrl}/login`,data,) //{ 'headers': this.headers }
+    return this.http.post(`${environment.apiUrl}/user/login`,data,) //{ 'headers': this.headers }
   }
  
 }
