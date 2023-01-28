760fcd244a65cf876a8dbb9594b89ca41013c3db
JitBherwani <jitbherwani92@gmail.com>
2023-01-28 19:59:19 +0530

    initial commit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,17 +2,45 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  token! :any ;
+  httpOptions:any;
+
   constructor(private http:HttpClient) { }
 
   // headers= new HttpHeaders()
   // .set('content-type', 'application/json')
   // .set('Access-Control-Allow-Origin', '*');
+
+  post(serviceName : string, data:any){
+    this.token =  localStorage.getItem('token');
+    if(this.token != null || this.token != undefined || this.token != "" ){
+      this.httpOptions = {
+        headers: new HttpHeaders({
+          "Access-Control-Allow-Origin": '*',
+          'Accept' : 'application/json, text/plain',
+          'Authorization' : `Bearer ${this.token}`
+        })
+      }
+    }else{
+      this.httpOptions = {
+        headers: new HttpHeaders({
+          "Access-Control-Allow-Origin": '*',
+          'Accept' : 'application/json, text/plain',
+        })
+      }
+    }
+
+    const url = environment.apiUrl + serviceName;
+    return  this.http.post(url,  data, this.httpOptions);
+
+  }
 
   signup(data:any):Observable<any>{
     return this.http.post('/signup',data)
