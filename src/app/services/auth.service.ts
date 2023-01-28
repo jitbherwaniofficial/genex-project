6b3ef24bb83374745d0d1a7e129300701880b071
JitBherwani <jitbherwani92@gmail.com>
2023-01-28 20:14:28 +0530

    initial commit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,8 +9,6 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class AuthService {
 
-  token! :any ;
-  httpOptions:any;
 
   constructor(private http:HttpClient) { }
 
@@ -18,36 +16,14 @@ export class AuthService {
   // .set('content-type', 'application/json')
   // .set('Access-Control-Allow-Origin', '*');
 
-  post(serviceName : string, data:any){
-    this.token =  localStorage.getItem('token');
-    if(this.token != null || this.token != undefined || this.token != "" ){
-      this.httpOptions = {
-        headers: new HttpHeaders({
-          "Access-Control-Allow-Origin": '*',
-          'Accept' : 'application/json, text/plain',
-          'Authorization' : `Bearer ${this.token}`
-        })
-      }
-    }else{
-      this.httpOptions = {
-        headers: new HttpHeaders({
-          "Access-Control-Allow-Origin": '*',
-          'Accept' : 'application/json, text/plain',
-        })
-      }
-    }
 
-    const url = environment.apiUrl + serviceName;
-    return  this.http.post(url,  data, this.httpOptions);
-
-  }
 
   signup(data:any):Observable<any>{
-    return this.http.post('/signup',data)
+    return this.http.post(environment.apiUrl+'/signup',data)
   }
 
   signin(data:any):Observable<any>{
-    return this.http.post('/login',data,) //{ 'headers': this.headers }
+    return this.http.post(environment.apiUrl+'/login',data,) //{ 'headers': this.headers }
   }
  
 }
