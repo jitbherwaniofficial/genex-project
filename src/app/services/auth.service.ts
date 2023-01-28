a52ae70ff9d4241b57d24ee41b68cbfe82d44a45
JitBherwani <jitbherwani92@gmail.com>
2023-01-28 17:44:27 +0530

    cors update

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,16 @@ export class AuthService {
 
   constructor(private http:HttpClient) { }
 
+  headers= new HttpHeaders()
+  .set('content-type', 'application/json')
+  .set('Access-Control-Allow-Origin', '*');
+
   signup(data:any):Observable<any>{
-    return this.http.post('https://genex-api.onrender.com/user/signup',data)
+    return this.http.post('https://genex-api.onrender.com/user/signup',data,{ 'headers': this.headers })
   }
 
   signin(data:any):Observable<any>{
-    return this.http.post('https://genex-api.onrender.com/user/login',data)
+    return this.http.post('https://genex-api.onrender.com/user/login',data,{ 'headers': this.headers })
   }
  
 }
