9c9c1e59bf218b328c7c9121876fc8c8e5feb48f
JitBherwani <jitbherwani92@gmail.com>
2023-01-29 14:39:49 +0530

    updated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,18 +12,18 @@ export class AuthService {
 
   constructor(private http:HttpClient) { }
 
-  headers= new HttpHeaders()
-  .set('content-type', 'application/json')
-  .set('Access-Control-Allow-Origin', '*');
+  // headers= new HttpHeaders()
+  // .set('content-type', 'application/json')
+  // .set('Access-Control-Allow-Origin', '*');
 
 
 
   signup(data:any):Observable<any>{
-    return this.http.post<any>(`${environment.apiUrl}}/user/signup`,data,{ 'headers': this.headers })
+    return this.http.post<any>(`${environment.apiUrl}}/user/signup`,data,)
   }
 
   signin(data:any):Observable<any>{
-    return this.http.post<any>(`${environment.apiUrl}/user/login`,data,{ 'headers': this.headers })
+    return this.http.post<any>(`${environment.apiUrl}/user/login`,data,) //{ 'headers': this.headers }
   }
  
 }
